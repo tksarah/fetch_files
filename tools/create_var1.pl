ecfde389ee13f22410dc60dc6594b3b30ba1d1d1
tksarah <tsarah8139@gmail.com>
2016-10-20 08:25:53 +0000

    Update 20161020_1748

--- tools/create_var1.pl
+++ tools/create_var1.pl
@@ -5,15 +5,14 @@ if($ARGV[0] eq ""){ print "Not found the file\n"; exit 1; }
 my $line;
 
 open(R,"$ARGV[0]");
-open(W,">./roles/fetch_file/vars/files.yml");
+	open(W,">./roles/fetch_file/vars/files.yml");
+	print W "lists:\n";
+	while (<R>){
+		chomp($_);
+		print W "  - { filename: '$_' }\n";
+		}	
 
-print W "lists:\n";
-while (<R>){
-	chomp($_);
-	print W "  - { filename: '$_' }\n";
-	}	
-
-close(W);
+	close(W);
 close(R);
 
 print "Created ./roles/fetch_file/vars/files.yml \n";
